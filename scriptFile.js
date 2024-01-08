14157e73da3e3086c98c5d1b479d3ab0dd1e34f0
evan <101075554+AgedFineWine@users.noreply.github.com>
2024-01-08 14:47:22 -0500

    removed game out of 5, added 3 buttons to chose Rock, paper, scissors

--- scriptFile.js
+++ scriptFile.js
@@ -1,24 +1,24 @@
-function getComputerChoice(){
+function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
-    if (choice === 0){
+    if (choice === 0) {
         return 'rock';
-    } else if (choice === 1){
+    } else if (choice === 1) {
         return 'paper';
     } else{
         return 'scissors';
     }
 }
 
-function playRound(playerSelection, computerSelection){
-    if (playerSelection === 'rock'){
-        if (computerSelection === 'rock'){
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === 'rock') {
+        if (computerSelection === 'rock') {
             return ['Tie! The computer chose rock', 0];
         } else if (computerSelection === 'paper'){
             return ['You Lose! The computer chose paper', 1];
         } else if (computerSelection === 'scissors'){
             return ['You Win! The computer chose scissors', 2];
         }
-    } else if (playerSelection === 'paper'){
+    } else if (playerSelection === 'paper') {
         if (computerSelection === 'rock'){
             return ['You Win! The computer chose rock', 2];
         } else if (computerSelection === 'paper'){
@@ -26,7 +26,7 @@ function playRound(playerSelection, computerSelection){
         } else if (computerSelection === 'scissors'){
             return ['You Lose! The computer chose scissors', 1];
         }
-    } else if (playerSelection === 'scissors'){
+    } else if (playerSelection === 'scissors') {
         if (computerSelection === 'rock'){
             return ['You Lose! The computer chose rock', 1];
         } else if (computerSelection === 'paper'){
@@ -37,7 +37,7 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
-function verdict(scoreCount, lossCount){
+function verdict(scoreCount, lossCount) {
     if (scoreCount > lossCount){
         console.log(`VERDICT: Player wins! You beat the computer ${scoreCount}/5 times!`);
     } else if (scoreCount === lossCount){
@@ -47,25 +47,35 @@ function verdict(scoreCount, lossCount){
     }
 }
 
-function game(){
+function game(playerSelection) {
     let scoreCount = 0;
     let lossCount = 0;
-    for (let start = 0; start < 5; start++){
-        let playerSelection = prompt('rock, paper, or scissors? ');
-        playerSelection = playerSelection.toLowerCase();
-        let computerSelection = getComputerChoice();
-        let resultArray = playRound(playerSelection, computerSelection);
-        if ((resultArray[1]) === 0){
-            console.log(resultArray[0], `[Player: ${scoreCount} / Computer: ${lossCount}]`);
-        } else if (resultArray[1] === 2){
-            scoreCount += 1;
-            console.log(resultArray[0], `[Player: ${scoreCount} / Computer: ${lossCount}]`);
-        } else if (resultArray[1] === 1){
-            lossCount += 1;
-            console.log(resultArray[0], `[Player: ${scoreCount} / Computer: ${lossCount}]`);
-        }
+    // playerSelection = playerSelection.toLowerCase();
+
+    let computerSelection = getComputerChoice();
+    let resultArray = playRound(playerSelection, computerSelection);
+    if ((resultArray[1]) === 0){
+        console.log(resultArray[0], `[Player: ${scoreCount} / Computer: ${lossCount}]`);
+    } else if (resultArray[1] === 2){
+        scoreCount += 1;
+        console.log(resultArray[0], `[Player: ${scoreCount} / Computer: ${lossCount}]`);
+    } else if (resultArray[1] === 1){
+        lossCount += 1;
+        console.log(resultArray[0], `[Player: ${scoreCount} / Computer: ${lossCount}]`);
     }
     verdict(scoreCount, lossCount);
 }
 
-game()
+function main() {
+    const rock = document.querySelector('#rock');
+    const paper = document.querySelector('#paper');
+    const scissors = document.querySelector('#scissors');
+
+    rock.addEventListener('click', () => game('rock'));
+    paper.addEventListener('click', () => game('paper'));
+    scissors.addEventListener('click', () => game('scissors'));
+
+}
+
+main()
+
